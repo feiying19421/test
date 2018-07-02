2eb90b0dbf6093fa8bc594fec331c367cbb35086
qingyun <qingyun@songerbo.local>
2018-07-02 10:43:05 +0800

    修稿

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("你好");
+		System.out.println("打猎");
+		System.out.println("1223");
+
+
+
+	}
+}
--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("你好,中国");
 		System.out.println("喝酒断片!");
+		System.out.println("世界");
 	}
 }
